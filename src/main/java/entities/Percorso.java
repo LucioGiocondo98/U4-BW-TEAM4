09b91f41faedc624e051fb63709fa8dd402f7acf
Barbara <barbyns95@gmail.com>
2025-05-26 19:36:59 +0200

    add class

--- src/main/java/entities/Percorso.java
+++ src/main/java/entities/Percorso.java
@@ -1,4 +1,23 @@
 package entities;
 
-public class Percorso {
-}
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+    @Entity
+    public class Percorso {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        private LocalDateTime orarioPartenza;
+        private LocalDateTime orarioArrivo;
+
+        @ManyToOne
+        private Mezzo mezzo;
+
+        @ManyToOne
+        private Tratta tratta;
+
+        // Getters and Setters
+    }
+
--- src/main/java/entities/Tratta.java
+++ src/main/java/entities/Tratta.java
@@ -1,4 +1,21 @@
 package entities;
 
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
 public class Tratta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String zonaPartenza;
+    private String capolinea;
+    private int tempoPrevistoMinuti;
+
+    @OneToMany(mappedBy = "tratta")
+    private List<Mezzo> mezzi;
+
+    // Getters and Setters
 }
+
