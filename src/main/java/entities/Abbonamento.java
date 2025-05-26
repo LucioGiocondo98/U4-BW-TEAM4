1f0ad27748fb67c394bc8e32455b19335efae61b
Pierattilio-correale <pierattiliocorreale@yahoo.it>
2025-05-26 16:21:21 +0200

    created class

---
+++ src/main/java/entities/Abbonamento.java
@@ -0,0 +1,39 @@
+package entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+
+import java.time.LocalDate;
+@Entity
+public class Abbonamento extends  TitoloDiViaggio {
+    @Enumerated(EnumType.STRING)
+    private Validita validita;
+
+    public Abbonamento(String codiceUnivoco, LocalDate dataEmissione, Validita validita) {
+        super(codiceUnivoco, dataEmissione);
+        this.validita = validita;
+    }
+
+    public Abbonamento() {
+    }
+
+    public Abbonamento(String codiceUnivoco, LocalDate dataEmissione) {
+        super(codiceUnivoco, dataEmissione);
+    }
+
+    public Validita getValidita() {
+        return validita;
+    }
+
+    public void setValidita(Validita validita) {
+        this.validita = validita;
+    }
+
+    @Override
+    public String toString() {
+        return "Abbonamento{" +
+                "validita=" + validita +
+                '}';
+    }
+}
---
+++ src/main/java/entities/Validita.java
@@ -0,0 +1,5 @@
+package entities;
+
+public enum Validita {
+    SETTIMANALE , MENSILE
+}
